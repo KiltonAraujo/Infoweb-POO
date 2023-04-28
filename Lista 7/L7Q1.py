d1e57f81d847988acf1d4467a53663ecf6ea1602
KinnKill <124375571+KinnKill@users.noreply.github.com>
2023-04-28 12:59:26 +0000

    pao

---
+++ Lista 7/L7Q1.py
@@ -0,0 +1,5 @@
+def maior(x, y):
+  print(max(x,y))
+  
+x,y = map(int,input().split())
+maior(x,y)
---
+++ Lista 7/L7Q2.py
@@ -0,0 +1,5 @@
+def maior(x, y, z):
+  print(max(x,y,z))
+  
+x,y,z = map(int,input().split())
+maior(x,y,z)
